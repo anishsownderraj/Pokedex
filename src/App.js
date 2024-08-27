b2221fc9a0f860b6e0960db7c5d45317ef30db66
anishsownderraj <toanish21@gmail.com>
2024-08-26 17:57:23 -0700

    Add footer to app js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import NavBar from './components/Navbar';
 import Header from './components/Header';
 import PokemonList from './components/PokemonList';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <NavBar />
             <Header />
             <PokemonList />
+            <Footer />
         </div>
     );
 };
