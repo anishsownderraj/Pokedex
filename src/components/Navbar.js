181d80cafd9c83f38d70120e51a248574c970404
anishsownderraj <toanish21@gmail.com>
2024-08-28 11:27:15 -0700

    Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,74 @@
+// dependencies
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+// icons
+import {
+    TiSocialInstagramCircular,
+    TiSocialLinkedinCircular,
+    TiSocialGithubCircular
+} from 'react-icons/ti';
+// images
+import PokemonLogo from '../assets/Pokemon-Logo.png';
+
+const Navbar = () => {
+    return (
+        <header>
+            <motion.nav>
+                <div className='nav-container'>
+                    <div className='nav-logo shine'>
+                        <Link>
+                            <motion.img
+                                src={PokemonLogo}
+                                className='logo'
+                                alt='logo'
+                                animate={{ opacity: 1.2, y: [20, 0], shine: 200 }}
+                            />
+                        </Link>
+                    </div>
+
+                    <div className='social-links'>
+                        <a
+                            href="https://www.instagram.com/_anishs_"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <motion.div
+                                animate={{ opacity: 1.2, y: [20, 0] }}
+                                transition={{ delay: 0.25 }}
+                            >
+                                <TiSocialInstagramCircular className="nav-icon" size="2em" />
+                            </motion.div>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/anish-sownderraj"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <motion.div
+                                animate={{ opacity: 1.2, y: [20, 0] }}
+                                transition={{ delay: 0.35 }}
+                            >
+                                <TiSocialLinkedinCircular className="nav-icon" size="2em" />
+                            </motion.div>
+                        </a>
+                        <a
+                            href="https://github.com/anishsownderraj"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <motion.div
+                                animate={{ opacity: 1.2, y: [20, 0] }}
+                                transition={{ delay: 0.45 }}
+                            >
+                                <TiSocialGithubCircular className="nav-icon" size="2em" />
+                            </motion.div>
+                        </a>
+                    </div>
+                </div>
+            </motion.nav>
+        </header >
+    );
+};
+
+export default Navbar;
