dcaa5c0ad761a5ea44a4471a4b08a476f5205d48
anishsownderraj <toanish21@gmail.com>
2024-08-28 17:04:45 -0700

    Remove unused dependency useref

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,5 +1,5 @@
 // dependencies
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 // components
 import PokemonCard from './PokemonCard';
